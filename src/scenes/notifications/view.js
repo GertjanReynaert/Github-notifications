415c2a19d5a1af12cf69ac92d4c57dfd66b22ab3
Gertjan Reynaert <gertjan@typework.com>
2017-02-08 13:07:24 +0100

    temp(WIP): work in progress [ci skip]

--- src/scenes/notifications/view.js
+++ src/scenes/notifications/view.js
@@ -1,9 +1,9 @@
 // @flow
 import React, { Component, PropTypes } from 'react';
-import { Image, Text, ActivityIndicator, StyleSheet } from 'react-native';
+import { View, ListView, ActivityIndicator, Text, Image, StyleSheet } from 'react-native';
 import { connect } from 'react-refetch';
 
-import notifications from '../../assets/notifications.png';
+import notificationsIcon from '../../assets/notifications.png';
 
 const styles = StyleSheet.create({
   icon: {
@@ -12,27 +12,57 @@ const styles = StyleSheet.create({
   },
 });
 
+type Props = {
+  notifications: {
+    pending: Boolean,
+    value: Array<Object>,
+  },
+};
+
 class Notifications extends Component {
-  static navigationOptions = {
-    tabBar: {
-      label: 'Notifications',
-      icon: ({ tintColor }) => (
-        <Image source={notifications} style={[styles.icon, { tintColor }]} />
-      ),
-    },
-  }
+  props: Props
 
   static contextTypes = {
     accessToken: PropTypes.string,
   }
 
+  static navigationOptions = {
+    tabBar: {
+      label: 'Notifications',
+      icon: ({ tintColor }) => (
+        <Image source={notificationsIcon} style={[styles.icon, { tintColor }]} />
+      ),
+    },
+  }
+
+  componentWillMount() {
+    this.dataSource = new ListView.DataSource({
+      rowHasChanged: (row1, row2) => row1.id !== row2.id,
+    });
+  }
+
   render() {
-    console.log(this.props.notifications);
     if (this.props.notifications.pending) {
       return <ActivityIndicator small />;
     }
 
-    return <Text style={{ padding: 25 }}>{JSON.stringify(this.props.notifications.value[0])}</Text>;
+    console.log(JSON.stringify(this.props.notifications.value[0], null, 2));
+
+    const notifications = this.dataSource.cloneWithRows(this.props.notifications.value);
+
+    return (
+      <View style={{ paddingTop: 25 }}>
+        <ListView
+          style={{}}
+          dataSource={notifications}
+          renderRow={
+            ({ subject }) => (
+              <Text>{subject.title}</Text>
+            )
+          }
+        />
+      </View>
+    );
   }
 }
 
