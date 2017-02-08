0f2aa1613c54d91df195398c5a18a47f2cbc2dd9
Gertjan Reynaert <gertjan@typework.com>
2017-02-08 09:44:55 +0100

    Improve tabs

--- src/scenes/profile/FollowersNavigationWrapper.js
+++ src/scenes/profile/FollowersNavigationWrapper.js
@@ -1,17 +1,33 @@
 // @flow
 import React, { Component } from 'react';
+import { Image, StyleSheet } from 'react-native';
 import Followers from './Followers';
+
+import account from '../../assets/account.png';
 
 type Props = {
   navigation: Object,
 };
+
+const styles = StyleSheet.create({
+  icon: {
+    width: 26,
+    height: 26,
+  },
+});
 
 class FollowersNavigationWrapper extends Component {
   props: Props
 
   static navigationOptions = {
     title: ({ state }) => state.params.user,
-  };
+    tabBar: {
+      label: 'Profile',
+      icon: ({ tintColor }) => (
+        <Image source={account} style={[styles.icon, { tintColor }]} />
+      ),
+    },
+  }
 
   render() {
     return (
